6031e30c9d99a564c520e17a67b2ebf4815d533c
slattgram <outta.hoodz@gmail.com>
2026-03-19 14:55:32 +0200

    save theme to cookie

--- src/app/shared/components/misc/change-theme-button/change-theme-button.ts
+++ src/app/shared/components/misc/change-theme-button/change-theme-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { Button } from 'primeng/button';
 
 @Component({
@@ -7,9 +7,55 @@ import { Button } from 'primeng/button';
   templateUrl: './change-theme-button.html',
   styleUrl: './change-theme-button.scss',
 })
-export class ChangeThemeButton {
+export class ChangeThemeButton implements OnInit {
+  isDark = signal(false);
+
+  ngOnInit() {
+    // In real world this would be a service that handles it but I decided to do it in component
+    // to keep it simple since this button is loaded in root anyway
+    const cookieTheme = this.getThemeFromCookie();
+    this.isDark.set(
+      cookieTheme !== null
+        ? cookieTheme
+        : window.matchMedia('(prefers-color-scheme: dark)').matches,
+    );
+
+    if (this.isDark()) {
+      document.querySelector('html').classList.add('app-dark');
+    }
+  }
+
   toggleDarkMode() {
+    this.isDark.update((value) => !value);
     const element = document.querySelector('html');
-    element.classList.toggle('app-dark');
+
+    if (this.isDark()) {
+      element.classList.add('app-dark');
+    } else {
+      element.classList.remove('app-dark');
+    }
+
+    this.saveThemePreferenceToCookie();
+  }
+
+  private saveThemePreferenceToCookie(): void {
+    const value = this.isDark() ? 'dark' : 'light';
+    const expires = new Date();
+    expires.setFullYear(expires.getFullYear() + 1);
+
+    document.cookie = `app-theme=${value}; expires=${expires.toUTCString()}; path=/; SameSite=Lax`;
+  }
+
+  private getThemeFromCookie(): boolean | null {
+    const cookies = document.cookie.split(';');
+
+    const themeCookie = cookies.find((cookie) => cookie.trim().split('=')[0]);
+
+    if (themeCookie) {
+      const [, value] = themeCookie.trim().split('=');
+      return value === 'dark';
+    }
+
+    return null;
   }
 }
